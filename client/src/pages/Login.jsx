2d1976b07ce459c038746e93fcee5a45fd7e3537
codewithmishra-ashish <work.mishraashish@gmail.com>
2025-12-18 17:05:52 +0545

    updated frontend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
   const { login } = useContext(AuthContext);
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600">GitVox</h1>
+      <h1 className="text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r"><span className="text-white">Git</span><span className="text-red-600">Vox</span></h1>
       <button onClick={login} className="flex items-center gap-3 bg-gray-800 px-8 py-4 rounded-xl text-xl font-bold hover:bg-gray-700 transition">
         <FaGithub /> Login with GitHub
       </button>
